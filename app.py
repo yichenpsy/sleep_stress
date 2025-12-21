3eea503d4e5035a17cdcf875125fd55ca75dd5ed
yichenzhong <yichenpsy@gmail.com>
2025-12-21 12:39:53 +0100

    refactoring

--- app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title('Meine erste Streamlit App! 🎉')
-st.write('Hello World!')
-st.write('Das ist sçuper einfach!')
---
+++ src/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title('Meine erste Streamlit App! 🎉')
+st.write('Hello World!')
+st.write('Das ist sçuper einfach!')
