9731dbe35476fd53220eb983ac99aa48e722bad7
yichenzhong <yichenpsy@gmail.com>
2026-01-07 12:12:13 +0100

    data clean

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('Meine erste Streamlit App! 🎉')
 st.write('Hello World!')
-st.write('Das ist sçuper einfach!')
+st.write('Das ist super einfach!')
--- stream_lit_test.py
+++ stream_lit_test.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import pandas as pd
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+DATA_PATH = BASE_DIR.parent / "data" / "processed" / "data_final.csv"
 
 @st.cache_data
 def load_data():
-    return pd.read_csv("data/sleep_clean.csv")
+    return pd.read_csv(DATA_PATH)
 
 df = load_data()
 
